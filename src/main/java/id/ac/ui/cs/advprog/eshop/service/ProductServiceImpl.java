db5d194c0eab4c7152b57ecdb85fb5cc869d65c8
aryanstya <aryanstya238@gmail.com>
2024-02-03 18:17:42 +0700

    [Feature Update]- Edit Product

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -17,10 +18,39 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product create (Product product) {
+        product.setProductId(String.valueOf(UUID.randomUUID()));
         productRepository.create(product);
         return product;
     }
 
+    @Override
+    public Product edit(String id, Product product) {
+        Iterator<Product> products = productRepository.findAll();
+
+        int index = 0;
+        for (; products.hasNext(); index++) {
+            Product currentProduct = products.next();
+            if (currentProduct.getProductId().equals(id)) {
+                product.setProductId(currentProduct.getProductId());
+                break;
+            }
+        }
+
+        return productRepository.replace(index, product);
+    }
+
+    @Override
+    public Product get(String id) {
+        Iterator<Product> products = productRepository.findAll();
+
+        while (products.hasNext()) {
+            Product currentProduct = products.next();
+            if (currentProduct.getProductId().equals(id)) {
+                return currentProduct;
+            }
+        }
+        return null;
+    }
     @Override
     public List <Product> findAll() {
         Iterator <Product> productIterator = productRepository.findAll();
@@ -28,4 +58,5 @@ public class ProductServiceImpl implements ProductService {
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
+
 }
