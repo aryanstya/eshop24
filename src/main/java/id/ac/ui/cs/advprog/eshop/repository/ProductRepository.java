1efebe7de686a9cb35b9f99b0f44b803d01a15dd
aryanstya <aryanstya238@gmail.com>
2024-02-24 01:56:05 +0700

    update LSP-ISP-DIP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -8,27 +8,45 @@ import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class ProductRepository {
-    private List <Product> productData = new ArrayList<>();
+public class ProductRepository implements ProductRepositoryInterface {
+    private List<Product> productData = new ArrayList<>();
 
-    public Product create (Product product) {
+    public void create(Product product) {
         productData.add(product);
-        return product;
-    }
-
-    public Iterator <Product> findAll() {
-        return productData.iterator();
-    }
-
-    public Product replace(int index, Product product) {
-        return productData.set(index, product);
     }
 
     public boolean delete(Product product) {
-
         return productData.remove(product);
-
     }
 
+    public Product edit(String id, Product product) {
+        Iterator<Product> products = findAll();
 
+        int index = 0;
+        for (; products.hasNext(); index++) {
+            Product currentProduct = products.next();
+            if (currentProduct.getProductId().equals(id)) {
+                product.setProductId(currentProduct.getProductId());
+                return productData.set(index, product);
+            }
+        }
+
+        return null;
+    }
+
+    public Product get(String id) {
+        Iterator<Product> products = findAll();
+
+        while (products.hasNext()) {
+            Product currentProduct = products.next();
+            if (currentProduct.getProductId().equals(id)) {
+                return currentProduct;
+            }
+        }
+        return null;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
 }
